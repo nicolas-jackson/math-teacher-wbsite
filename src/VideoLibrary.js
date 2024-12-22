d71b68f45b32c28c6ae7f842b0258b1003cc843d
nicolas <madiyar.pmk@mail.ru>
2024-12-22 20:03:57 +0500

    Updated API URL to production

--- src/VideoLibrary.js
+++ src/VideoLibrary.js
@@ -10,7 +10,7 @@ const VideoLibrary = () => {
   useEffect(() => {
     const fetchVideos = async () => {
       try {
-        const response = await axios.get("http://localhost:5000/api/videos");
+        const response = await axios.get("https://math-teacher-website.onrender.com/api/videos");
         setVideos(response.data); // Store videos in state
       } catch (error) {
         console.error("Error fetching videos:", error);
